229b31e339afbbe7f37123e4c73cf20f46741ab2
Emmanuel James <chiderajames55@gmail.com>
2023-07-23 15:54:50 +0100

    feat: Add beautification of HTML code in textarea

--- README.md
+++
@@ -1,2 +0,0 @@
-# HTMLToText
-HTMLToText is a lightweight and secure web application that allows users to input HTML content and securely store it in the backend. The stored HTML content can later be retrieved and displayed as text on the user's view, ensuring a safe and seamless experience.
--- app.js
+++ app.js
@@ -1,42 +1,12 @@
-const express = require("express");
-const shortid = require("shortid");
+function convertToText() {
+    const htmlInput = document.getElementById("htmlInput").value;
+    const textOutput = document.getElementById("textOutput");
+    textOutput.innerHTML = htmlInput;
+}
 
-const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// In-memory data store to store the converted HTML and short links
-const htmlData = {};
-
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
-});
-
-app.post("/convert", (req, res) => {
-    const htmlInput = req.body.html_content;
-    const shortLink = shortid.generate();
-
-    // Store the HTML content and its short link in the data store
-    htmlData[shortLink] = htmlInput;
-
-    // Return the short link as JSON response
-    res.json({ short_link: shortLink });
-});
-
-app.get("/:shortLink", (req, res) => {
-    const shortLink = req.params.shortLink;
-    const convertedHtml = htmlData[shortLink];
-
-    if (convertedHtml) {
-        // Send the converted HTML content as the response
-        res.send(convertedHtml);
-    } else {
-        // Short link not found, return a 404 error
-        res.sendStatus(404);
-    }
-});
-
-const port = 3000;
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+function resetInput() {
+    const htmlInput = document.getElementById("htmlInput");
+    const textOutput = document.getElementById("textOutput");
+    htmlInput.value = "";
+    textOutput.innerHTML = "";
+}
